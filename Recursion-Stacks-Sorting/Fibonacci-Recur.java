449ec3f8fce09d6fbb8d32e4d6cd8d8f83218f6c
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-09-27 15:53:19 -0400

    Update Fibonacci-Recur.java
    Finished

--- Recursion-Stacks-Sorting/Fibonacci-Recur.java
+++ Recursion-Stacks-Sorting/Fibonacci-Recur.java
@@ -1,7 +1,7 @@
 package StucturesClass;
 import java.util.Scanner;
 
-public class Lab6FibonacciSequence {
+public class Lab6FibonacciSequenceFINISHED {
     public static int findFibonacciRecursive(int n){
         if(n == 0){
             return 0;}
@@ -9,16 +9,20 @@ public class Lab6FibonacciSequence {
             return 1;}
         return findFibonacciRecursive(n-1)+findFibonacciRecursive(n-2);
     }
-    public static int findFibonacciIterative(int n){ //fix this
-        if(n == 0){
-            return 0;}
-        else if(n==1){
-            return 1;}
-        int sum=0;
-        for (int i=n-1;i<=n;i++){
-            sum +=i;
+    public static int findFibonacciIterative(int n){
+        int n1=0,n2=1,n3;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        n3=n2+n1;
+        for(int i=2;i<n;i++)
+        {
+            n1=n2;
+            n2=n3;
+            n3=n2+n1;
         }
-        return sum;
+        return n3;
     }
 
     public static void main(String[] args) {
