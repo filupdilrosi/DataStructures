429f585c1782832b717aede14322e98dcccce067
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-10-01 14:05:40 -0400

    Create selectionSort.java
    o(n^2)

---
+++ Recursion-Stacks-Sorting/sorts/selectionSort.java
@@ -0,0 +1,50 @@
+package javaTests;
+
+import java.util.Random;
+
+public class selectionSort {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(1000000);
+        }
+
+        System.out.println("Before:");
+        printArray(numbers);
+
+        selectionSort(numbers);
+
+
+        System.out.println("\nAfter:");
+        printArray(numbers);
+    }
+
+    public static int[] selectionSort(int[] list) {
+        int i, j, minValue, minIndex, temp = 0;
+        for (i = 0; i < list.length; i++) {
+            minValue = list[i];
+            minIndex = i;
+            for (j = i; j < list.length; j++) {
+                if (list[j] < minValue) {
+                    minValue = list[j];
+                    minIndex = j;
+                }
+            }
+            if (minValue < list[i]) {
+                temp = list[i];
+                list[i] = list[minIndex];
+                list[minIndex] = temp;
+            }
+        }
+        return list;
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
