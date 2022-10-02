254c18aba8bb6f6eaef62851ef1ff617fc400a24
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-10-02 16:59:21 -0400

    Create StackException.java

---
+++ Recursion-Stacks-Sorting/stacks/StackException.java
@@ -0,0 +1,8 @@
+package Lec6Stacks_end;
+
+public class StackException
+        extends java.lang.RuntimeException {
+    public StackException(String s) {
+        super(s);
+    } // end constructor
+} // end StackException
