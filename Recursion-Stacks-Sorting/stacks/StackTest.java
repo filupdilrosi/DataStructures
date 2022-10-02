db7e9941075a054e27bae1ae3128e06612222e9f
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-10-02 17:00:14 -0400

    Create StackTest.java

---
+++ Recursion-Stacks-Sorting/stacks/StackTest.java
@@ -0,0 +1,20 @@
+package Lec6Stacks_end;
+
+public class StackTest {
+    public static final int MAX_ITEMS = 15;
+
+    public static void main(String[] args) {
+        StackArrayBased stack = new StackArrayBased();
+        Integer items[] = new Integer[MAX_ITEMS];
+        for (int i = 0; i < MAX_ITEMS; i++) {
+            items[i] = Integer.valueOf(i);
+            if (!stack.isFull()) {
+                stack.push(items[i]);
+            } // end if
+        } // end for
+        while (!stack.isEmpty()) {
+            // cast result of pop to Integer
+            System.out.println((Integer) (stack.pop()));
+        } // end while
+    }
+}
